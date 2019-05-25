20a88f551ea7103514c3f148607cc3e5d554324d
GuanYangCLU <guanyang@callutheran.edu>
2019-05-24 20:52:49 -0700

    0524_07

--- config/config.js
+++ config/config.js
@@ -1,8 +1,6 @@
-const db = {
+module.exports = {
   mongoURI: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
   githubClientId: process.env.GITHUB_CLIENT_ID,
   githubSecret: process.env.GITHUB_SECRET
 };
-
-module.exports = db;
